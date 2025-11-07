0802e160de237942c0a58847e03b0fa9ff1dae5b
Yang-Hyeon-Seo <ihyeonseo@naver.com>
2025-11-07 18:07:40 +0900

    feature-13-index

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 
 app_name='post'
 urlpatterns = [
-    
+    path("", views.index),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+    전체 금융 게시글 목록 조회 및 출력
+    """
+    context = {
+        
+    }
+    return render(request, 'index.html', context)
